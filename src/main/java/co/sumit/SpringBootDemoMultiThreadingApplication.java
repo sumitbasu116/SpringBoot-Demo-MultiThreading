7eb5cb41aa39ade49b127da8387af3a27d604a11
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2024-11-20 12:28:08 +0530

    First commit

---
+++ src/main/java/co/sumit/SpringBootDemoMultiThreadingApplication.java
@@ -0,0 +1,13 @@
+package co.sumit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoMultiThreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoMultiThreadingApplication.class, args);
+	}
+
+}
